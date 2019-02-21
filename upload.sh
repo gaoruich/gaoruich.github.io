0ae10e5beb3649ba1271ff53a2a2100b63a296ea
gaoruich <2310064298@qq.com>
2019-02-21 14:48:44 +0800

    modify post

---
+++ upload.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+git config --local user.name "gaoruich"
+git config --local user.email "2310064298@qq.com"
+git add -A
+git commit -m "modify post"
+git push git@github.com:gaoruich/gaoruich.github.io.git raw
+hexo clean | hexo g -d
