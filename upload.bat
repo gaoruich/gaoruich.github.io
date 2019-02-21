83587478e03bfe99e3ea0b1d62a57fa32ffe73ca
gaoruich <2310064298@qq.com>
2019-02-21 14:33:46 +0800

    modify post

--- upload.bat
+++ upload.bat
@@ -2,6 +2,6 @@ git config --local user.name "gaoruich"
 git config --local user.email "2310064298@qq.com"
 git add -A
 git commit -m "modify post"
-
+git push git@github.com:gaoruich/gaoruich.github.io.git source
 hexo clean | hexo g -d
 pause
